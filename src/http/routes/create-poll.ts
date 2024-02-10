dc30b3378ce9a5d31c9c8e66c3194be38e34e27e
j4marcos <joaomarcos2004.jonh@gmail.com>
2024-02-09 23:18:45 -0300

    feat: vote poll route

---
+++ src/http/routes/create-poll.ts
@@ -0,0 +1,37 @@
+import {z} from "zod"
+import {prisma} from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function createPoll(app : FastifyInstance) {
+
+    app.post("/polls", async (request, reply) => {
+        const createPollBody = z.object({
+            title : z.string(),
+            options: z.array(z.string())
+        })
+    
+        const { title, options } = createPollBody.parse(request.body)
+    
+        const poll = await prisma.poll.create({
+            data: {
+                title,
+                options: {
+                    createMany: {
+                        data: options.map(option => {
+                            return {title: option }
+                        })
+                    }
+                }
+            }
+        })
+
+        await prisma .pollOption.createMany ({
+            data: options.map(option => {
+                return { title: option, pollId: poll.id }
+            })
+        })
+    
+        console.log(request.body)
+        return reply.status(201).send({pollId : poll.id})
+    })
+}
